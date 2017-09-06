d745157e66421afe7af77662eac91532dd6025ff
Virendra Singh <virendra.singh.mca@gmail.com>
2017-09-06 17:26:50 +0530

    Adding all working file

---
+++ FirstProject/src/app/products/product-list.components.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core/';
+
+@Component({
+    selector:'pm-products',
+    templateUrl:'./product-list.components.html'
+
+})
+
+export class ProductListComponent{
+
+}
